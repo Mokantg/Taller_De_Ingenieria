18d9fcf0606abb73b3c037c9037b63e98d3cf3c5
Mokantg <129112579+Mokantg@users.noreply.github.com>
2026-06-11 20:53:45 +0000

    Estructura base de index.php

--- index.php
+++ index.php
@@ -2,34 +2,35 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Proyecto CRUD - Grupo X</title>
+    <title>Proyecto CRUD</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="container mt-5">
 
-    <h1>Proyecto: [Nombre de tu Aplicación]</h1>
+    <h1>Proyecto: Lista de tareas pendientes</h1>
 
     <section class="mt-4">
         <h2>Integrantes:</h2>
         <ul>
-            <li>Nombre del Integrante 1</li>
-            <li>Nombre del Integrante 2</li>
-            <li>Nombre del Integrante 3</li>
+            <li>Ricardo González</li>
+            <li>Monserrat Palma</li>
+            <li>Javier Quezada</li>
+            <li>Felipe Rogel</li>
         </ul>
     </section>
 
     <section class="mt-4">
         <h2>Descripción de la Aplicación:</h2>
-        <p>Esta aplicación consiste en [describir brevemente qué hace tu app, ej: un sistema de inventario, una agenda de contactos, un gestor de tareas].</p>
+        <p>Permite a los usuarios gestionar sus pendientes de manera eficiente a través de una interfaz dinámica, donde cada tarea actúa como una unidad de información independiente que puede ser creada, consultada, modificada o eliminada en tiempo real. </p>
     </section>
 
     <section class="mt-4">
         <h2>Operaciones CRUD a implementar:</h2>
         <ul>
-            <li><strong>Create (Crear):</strong> Registro de nuevos datos en la base de datos a través de formularios.</li>
-            <li><strong>Read (Leer):</strong> Visualización de los datos almacenados en una tabla.</li>
-            <li><strong>Update (Actualizar):</strong> Modificación de registros existentes para mantener la información al día.</li>
-            <li><strong>Delete (Eliminar):</strong> Eliminación segura de registros que ya no se necesiten.</li>
+            <li><strong>Create (Crear):</strong> Añadir una nueva tarea.</li>
+            <li><strong>Read (Leer):</strong> Ver lista de tareas pendientes.</li>
+            <li><strong>Update (Actualizar):</strong> Marcar una tarea como Completada o editar nombre de la tarea.</li>
+            <li><strong>Delete (Eliminar):</strong> Borrar una tarea terminada.</li>
         </ul>
     </section>
 
